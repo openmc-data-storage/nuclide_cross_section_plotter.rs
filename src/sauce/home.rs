7fafcb2539064ff7547d715a177778c0ac25610d
shimwell <mail@jshimwell.com>
2025-02-09 22:34:10 +0100

    added compact csv

--- src/sauce/home.rs
+++ src/sauce/home.rs
@@ -465,7 +465,7 @@ pub fn home() -> Html {
                     data={paginated_data} 
                     orderable={true}
                 />
-                <h5>{"Number selected"} <span class="badge text-bg-secondary">{sum}</span></h5>
+                <h5>{"Selected"} <span class="badge text-bg-secondary">{sum}</span>{" from 41337 available database entries."}</h5>
                 </div>
                 <div class="flex-grow-1 p-2 input-group">
 
